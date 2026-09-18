6237e456351c67fda0d10feafe97e3d007b01dc9
khacquitvu99 <khacquitvu99@gmail.com>
2026-09-19 00:42:59 +0700

    clear

--- app/(pages)/detail-acc/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TypeJob() {
-  return (
-    <div>
-      <h1>Type Job</h1>
-      <p>This is the type job page.</p>
-    </div>
-  );
-}
--- app/(pages)/detail-job/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DetailJob  () {
-    return (
-        <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-4">detail job header</div>
-            <div className="flex flex-col gap-4">detail job body</div>
-        </div>
-    )
-}
