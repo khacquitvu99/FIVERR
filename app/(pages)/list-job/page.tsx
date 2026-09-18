aba4649f8b152ffdf138c3034d40fa6db96e015e
khacquitvu99 <khacquitvu99@gmail.com>
2026-09-19 01:13:12 +0700

    update

--- app/(pages)/list-job/page.tsx
+++ app/(pages)/list-job/page.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from "react";
 
-import ListJobBody from "../../component/list-job/body";
-import ListJobHeader from "../../component/list-job/nav";
-import Filter from "../../component/list-job/filter";
-
+import ListJobBody from "@/component/list-job/body";
+import ListJobHeader from "@/component/list-job/nav";
+import Filter from "@/component/list-job/filter";
 export default function ListJob() {
   return (
     <Suspense fallback={<div className="text-center py-16">Đang tải...</div>}>
-      <ListJobBody />
-
       <ListJobHeader />
       <Filter />
       <ListJobBody />
