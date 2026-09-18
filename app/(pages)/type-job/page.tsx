9c678e9751da03a7ea206fb16c97f8aef74f38d8
khacquitvu99 <khacquitvu99@gmail.com>
2026-09-19 01:06:06 +0700

    update

--- app/(pages)/type-job/page.tsx
+++ app/(pages)/type-job/page.tsx
@@ -1,14 +1,18 @@
+import { Suspense } from "react";
+
 import JobType from "@/component/type-job/CategorySlider";
 import Navbar from "@/component/list-job/nav";
 import Exploder from "@/component/type-job/body-typejob/index";
 import Footer from "@/component/type-job/foot/index";
 export default function TypeJobPage() {
   return (
+    <Suspense>
     <div className="w-full min-h-screen bg-white">
       <Navbar />
       <JobType />
       <Exploder />
       <Footer />
     </div>
+    </Suspense>
   );
 }
